ee60c539a6013eabab9f9f411ca3c069edd9ae0b
jasom98@gmail.com <jasom98@gmail.com>
2020-11-22 15:10:54 -0500

    Borrado

--- app/src/main/java/com/example/loginmain/Model/myadapter.java
+++ app/src/main/java/com/example/loginmain/Model/myadapter.java
@@ -1,5 +1,7 @@
 package com.example.loginmain.Model;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -91,6 +93,34 @@ public class myadapter extends FirebaseRecyclerAdapter<Persona,myadapter.myviewh
 
                         }
                     });
+                }
+            });
+
+            //borrardatos
+            holder.delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(holder.delete.getContext());
+                    builder.setTitle("Ventana de Borrado");
+                    builder.setMessage("Seguro quieres borrar el registro?");
+
+                    builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+                            FirebaseDatabase.getInstance().getReference().child("Reportes")
+                                    .child(getRef(position).getKey())
+                                    .removeValue();
+                        }
+                    });
+
+                    builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    builder.show();
                 }
             });
 
