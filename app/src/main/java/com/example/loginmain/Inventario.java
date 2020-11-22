5a9e499af09f935bd0abf0977e13483a0c374908
jasom98@gmail.com <jasom98@gmail.com>
2020-11-22 13:59:40 -0500

    Buscador

--- app/src/main/java/com/example/loginmain/Inventario.java
+++ app/src/main/java/com/example/loginmain/Inventario.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.example.loginmain.Model.Persona;
 import com.example.loginmain.Model.myadapter;
@@ -43,5 +46,42 @@ public class Inventario extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //instanciar menu creado y realizacion de buscador
+        getMenuInflater().inflate(R.menu.search,menu);
+
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView  = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                busqueda(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                busqueda(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void busqueda(String s) {
+        FirebaseRecyclerOptions<Persona> options =
+                new FirebaseRecyclerOptions.Builder<Persona>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Reportes")
+                                .orderByChild("Correo").startAt(s).endAt(s+"\uf8ff"), Persona.class)
+                        .build();
+
+        adapter = new myadapter(options);
+        adapter.startListening();
+        reciview.setAdapter(adapter);
     }
 }
